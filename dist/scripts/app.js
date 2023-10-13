5816e0fe3b8a1ac4f71fa842f3f3fe0497da440c
Ferdlcz <ferdlcskate@gmail.com>
2023-10-12 23:49:24 -0600

    Navbar responsive y modo oscuro funcionando

---
+++ dist/scripts/app.js
@@ -0,0 +1,61 @@
+const navButton = document.querySelector("#menu-button");
+
+const menuItems = document.querySelector("#menu");
+
+const menuButtonSpan = document.querySelectorAll("#menu-button span");
+
+const enlaces = document.querySelectorAll("#menu a");
+
+//funcionalidad a boton de navbar movil
+navButton.addEventListener("click", () => {
+  menuItems.classList.toggle("hidden");
+
+  menuButtonSpan.forEach((span) => {
+    span.classList.toggle("anim");
+  });
+});
+
+//Cerrar menu al dar click en un enlace
+enlaces.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuItems.classList.add("hidden");
+
+    menuButtonSpan.forEach((span) => {
+      span.classList.remove("anim");
+    });
+  });
+});
+
+//DARK MODE
+
+//Verificar si el tema almacenado en localstorage o si el tema del sistema es dark, si es dark agrega la clase dark
+//si el tema es claro, la elimina 
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+} else {
+    document.documentElement.classList.remove('dark')
+}
+
+const darkButton = document.querySelector("#darkButton");
+
+//evento de click para cambiar el tema del color
+darkButton.addEventListener("click", function () {
+
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+});
